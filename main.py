ff265f35c3fc477d41b13e8126f713696dcd9af7
Beatle <spicezhukovsky@gmail.com>
2026-09-20 13:25:31 +0300

    Build def create report one compani

--- main.py
+++ main.py
@@ -20,15 +20,14 @@ from docx.shared import Cm, Pt
 work_in_dict = create_dict(data_list_chek, questions)
 transformed_dict = data_transform(work_in_dict)
 
-# Тут все отфильтровалось по id школы
-filterest_all_sh = filter_full_schools(transformed_dict)
-
-
 
 def currunt_month(data_dict, data_start, data_end):
+
+    filterest_sh = filter_full_schools(data_dict)
+
     # Тут отфильтрованны все просмотры до дате
     # Эта переменная будет хранить актуальный месяц
-    currunt_month = datatime_filter(data_dict, data_start, data_end)
+    currunt_month = datatime_filter(filterest_sh, data_start, data_end)
 
     # Тут мы создаем словарь со данными по каждому тренеру и рейтинг
     coach_data_currunt_month, coach_rank_currunt_month = filter_by_coach_in_school(currunt_month, create_over_rank_coach_sort=True)
@@ -40,9 +39,12 @@ def currunt_month(data_dict, data_start, data_end):
 
 
 def past_month(data_dict, data_start, data_end):
+
+    filterest_sh = filter_full_schools(data_dict)
+
     # Тут отфильтрованны все просмотры до дате
     # Эта переменная будет хранить прошлый месяц
-    past_month = datatime_filter(data_dict, data_start, data_end)
+    past_month = datatime_filter(filterest_sh, data_start, data_end)
 
     # Тут мы создаем словарь со данными по каждому тренеру и рейтинг
     coach_data_past_month, coach_rank_past_month = filter_by_coach_in_school(past_month, create_over_rank_coach_sort=True)
@@ -91,4 +93,44 @@ def create_all_sh_raport(name_doc, name_dict: dict, data_dict: dict, data_start:
         print(f'ОТЧЕТ_МОНИТОРИНГ {name}.docx')
 
 
-create_all_sh_raport("raport_biha", school_name, filterest_all_sh, "2023-09-01", "2024-03-01", "2024-02-01")
+def create_one_sh_raport(name_doc, name_dict: dict, name_sh, data_dict: dict, data_start: str, data_end_current_month: str, data_end_past_month: str):
+
+    for id_s, name in name_dict.items():
+        if name == name_sh:
+            id_sh = id_s
+            break
+
+
+    rank_all_sh_sort, coach_data_currunt_month, coach_rank_currunt_month = currunt_month(data_dict, data_start, data_end_current_month)
+    rank_all_sh_sort_past, coach_data_past_month, coach_rank_past_month = past_month(data_dict, data_start, data_end_past_month)
+
+    try:
+        dict_one_sh = rank_all_sh_sort[id_sh]
+        coach_one_sh = coach_data_currunt_month[id_sh]
+    except KeyError:
+        return f"Мониторинг УТЗ в Школе {name_sh} в период с {data_start} по {data_end_current_month} не осуществлялся."
+
+    name_doc = Document()
+    section = name_doc.sections[0]
+    section.footer_distance = Cm(0.2)
+
+    section.left_margin = Cm(1.5)
+    section.right_margin = Cm(1.5)
+    section.top_margin = Cm(1)
+    section.bottom_margin = Cm(1)
+
+    style = name_doc.styles['Normal']
+    font = style.font
+    font.name = 'Times New Roman'
+    font.size = Pt(14)
+
+    try:
+        sh_block(name_doc, dict_one_sh, rank_all_sh_sort, name_sh, id_sh, name_dict, rank_all_sh_sort_past)
+    except KeyError:
+        sh_block(name_doc, dict_one_sh, rank_all_sh_sort, name_sh, id_sh, name_dict)
+
+    over_coach_block(name_doc, coach_one_sh, coach_rank_currunt_month, name_sh)
+
+
+    name_doc.save(f'ОТЧЕТ_МОНИТОРИНГ {name}.docx')
+    print(f'ОТЧЕТ_МОНИТОРИНГ {name}.docx')
